8a0df82ea001bc0200dd04a04490645f9602df6c
Tom Shafer <tj@tjshafer.com>
2016-04-25 23:00:21 -0700

    Return all plans.
    Currently its not returning team plans. This will return team and regular plans.

--- src/Http/Controllers/PlanController.php
+++ src/Http/Controllers/PlanController.php
@@ -7,12 +7,12 @@ use Laravel\Spark\Spark;
 class PlanController extends Controller
 {
     /**
-     * Get the all of the plans defined for the appliation.
+     * Get the all of the plans defined for the application.
      *
      * @return Response
      */
     public function all()
     {
-        return response()->json(Spark::plans());
+        return response()->json(Spark::allPlans());
     }
 }
