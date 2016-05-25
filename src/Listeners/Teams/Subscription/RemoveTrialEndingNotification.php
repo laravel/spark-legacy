413d420a05ebb83da27eeafb864ae5b0d8a6e903
Daniel Jones <dannydjones1994@yahoo.co.uk>
2016-05-25 21:20:03 +0100

    Fix issue #79 - Trial period notification still subscription.
    Add new column to notifications table to store the event the
    notification is related to. This is used to track down a notification
    at a later date which was generated for a specific event.
    
    Update CreateTrialEndingNotification for single users and teams to
    complete the event column on the notification.
    
    Update CreateInvitationNotification for teams to complete the event
    column on the notification.
    
    Create RemoveTrialEndingNotification for single users and teams to
    remove the Trial Ending Notification create at registration.

---
+++ src/Listeners/Teams/Subscription/RemoveTrialEndingNotification.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Laravel\Spark\Listeners\Teams\Subscription;
+
+use Laravel\Spark\Spark;
+use Laravel\Spark\Events\Teams\Subscription\TeamSubscribed;
+use Laravel\Spark\Contracts\Repositories\NotificationRepository;
+
+class CreateTrialEndingNotification
+{
+    /**
+     * Handle the event.
+     *
+     * @param  TeamSubscribed  $event
+     * @return void
+     */
+    public function handle(TeamSubscribed $event)
+    {
+        if (! Spark::teamTrialDays()) {
+            return;
+        }
+
+        $notification = Notification::where('user_id', $event->team->owner->id)
+                      ->where('event', 'TeamRegistered')
+                      ->first()
+                      ->delete();
+    }
+}
