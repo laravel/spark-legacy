d5637a356813d863afe1d7eb146a54822266b48a
ionutz2k2 <ionutz2k2@gmail.com>
2016-06-16 21:06:00 +0300

    Update SendSupportEmail.php
    Added a check to verify if the 'from' data field is set and adds it to the e-mail message.

--- src/Interactions/Support/SendSupportEmail.php
+++ src/Interactions/Support/SendSupportEmail.php
@@ -36,7 +36,10 @@ class SendSupportEmail implements Contract
 
         Mail::raw($data['message'], function ($m) use ($data) {
             $m->to(Spark::supportAddress())->subject('Support Request: '.$data['subject']);
-            $m->from($data['from']);
+            
+            if (isset($data['from']))
+                $m->from($data['from']);
+            
             $m->replyTo($data['from']);
         });
     }
