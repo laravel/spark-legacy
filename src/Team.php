56999a0f2970747e27e24cad00cbe43813d4d00d
Jmrich <jmrichardsonjr@gmail.com>
2016-07-01 18:27:00 -0700

    team model fix
    Added team_id to Team relationships so that things work out the box if
    a model other than Team is used

--- src/Team.php
+++ src/Team.php
@@ -94,7 +94,7 @@ class Team extends Model
      */
     public function invitations()
     {
-        return $this->hasMany(Invitation::class);
+        return $this->hasMany(Invitation::class, 'team_id');
     }
 
     /**
@@ -104,7 +104,7 @@ class Team extends Model
      */
     public function subscriptions()
     {
-        return $this->hasMany(TeamSubscription::class)->orderBy('created_at', 'desc');
+        return $this->hasMany(TeamSubscription::class, 'team_id')->orderBy('created_at', 'desc');
     }
 
     /**
