77a9169ecac36258248acefa855ef9ea737c69c5
Mohamed Said <themohamedsaid@gmail.com>
2016-09-28 20:00:18 +0200

    remove form processing actions

--- resources/assets/js/kiosk/create-user.js
+++ resources/assets/js/kiosk/create-user.js
@@ -24,15 +24,11 @@ module.exports = {
          * Attempt to create a new user profile.
          */
         createUser() {
-            this.form.startProcessing();
-
             Spark.post('/spark/kiosk/users/create', this.form)
                 .then(response => {
                     this.userCreated = true;
 
                     this.password = response.password;
-
-                    this.form.finishProcessing();
 
                     this.form.name = '';
                     this.form.email = '';
