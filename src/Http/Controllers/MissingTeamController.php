977fe455bda930c0caa4180bc0ffa42f89af80b5
Jean-Philippe Murray <jpmurray@users.noreply.github.com>
2016-10-23 07:58:43 -0400

    /missing-team showing to non logged users
    I felt like it didn't make any sense that anyone could access `/missing-team` to be told they could go to their settings and then asked to be logged in. If I'm the only one to agree, let's close this proposal :)

--- src/Http/Controllers/MissingTeamController.php
+++ src/Http/Controllers/MissingTeamController.php
@@ -5,6 +5,16 @@ namespace Laravel\Spark\Http\Controllers;
 class MissingTeamController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
      * Show the missing team notice.
      *
      * @return Response
