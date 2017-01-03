43f2d516f2975a01c5cd6e4349aca5c5c850458d
Piotr Moćko <mocko.piotr@gmail.com>
2017-01-03 22:17:10 +0100

    Update CreateTrialEndingNotification.php

--- src/Listeners/Teams/Subscription/CreateTrialEndingNotification.php
+++ src/Listeners/Teams/Subscription/CreateTrialEndingNotification.php
@@ -34,7 +34,7 @@ class CreateTrialEndingNotification
      */
     public function handle(TeamCreated $event)
     {
-        if (! Spark::teamTrialDays() or $event->team->trial_ends_at === null) {
+        if (! Spark::teamTrialDays() || ! $event->team->trial_ends_at) {
             return;
         }
 
