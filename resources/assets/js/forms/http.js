591101ac86f28f70b01e21f752c68b009d61b82f
Artiik-fr <mathieu.artiikamcgaming@gmail.com>
2017-04-19 01:43:42 +0200

    Update http.js
    Use the setErrors function from the SparkForm class.

--- resources/assets/js/forms/http.js
+++ resources/assets/js/forms/http.js
@@ -47,8 +47,7 @@ module.exports = {
                     resolve(response.data);
                 })
                 .catch(errors => {
-                    form.errors.set(errors.response.data);
-                    form.busy = false;
+                    form.setErrors(errors.response.data);
 
                     reject(errors.data);
                 });
