c936bc256c148d16f7d3a400924eb8bb5023d8f6
Amir Hassan Azimi <hassanazimi@users.noreply.github.com>
2017-10-03 17:50:06 +0100

    Remove copying sweetalert.css
    In the new version of the sweetalert there is no css file. Users need to make it themselves.

--- install-stubs/webpack.mix.js
+++ install-stubs/webpack.mix.js
@@ -14,7 +14,6 @@ var path = require('path');
 
 mix.less('resources/assets/less/app.less', 'public/css')
    .copy('node_modules/sweetalert/dist/sweetalert.min.js', 'public/js/sweetalert.min.js')
-   .copy('node_modules/sweetalert/dist/sweetalert.css', 'public/css/sweetalert.css')
    .js('resources/assets/js/app.js', 'public/js')
    .webpackConfig({
         resolve: {
