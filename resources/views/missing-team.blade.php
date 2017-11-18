a900e2737d4e20dbcb64f11fdf364f341e3e72e1
Braunson Yager <Braunson@users.noreply.github.com>
2017-11-18 17:01:14 -0500

    Update missing-team.blade.php
    Added logic to check if the user can create teams or not and if not to not display the 'You can create one ...' in the `missing-team.blade.php` view. From Issue #848

--- resources/views/missing-team.blade.php
+++ resources/views/missing-team.blade.php
@@ -8,8 +8,10 @@
                 <div class="panel-heading">Where's Your {{ ucfirst(Spark::teamString()) }}?</div>
 
                 <div class="panel-body">
-                    It looks like you're not part of any {{ Spark::teamString() }}! You can create one in your
-                    <a href="/settings#/{{ str_plural(Spark::teamString()) }}">settings</a>.
+                    It looks like you're not part of any {{ Spark::teamString() }}!
+                    @if (Spark::createsAdditionalTeams()) 
+                        You can create one in your <a href="/settings#/{{ str_plural(Spark::teamString()) }}">settings</a>.
+                    @endif
                 </div>
             </div>
         </div>
