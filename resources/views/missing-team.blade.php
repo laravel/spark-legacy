8a3de22717b75a7a2040710832925d7745b5729a
Mohamed Said <themohamedsaid@gmail.com>
2017-11-22 17:29:17 +0200

    fix wording

--- resources/views/missing-team.blade.php
+++ resources/views/missing-team.blade.php
@@ -12,10 +12,10 @@
                     Where's Your {{ ucfirst(Spark::teamString()) }}?
                 </h2>
                 <p class="intro-copy">
-                    It looks like you're not part of any {{ Spark::teamString() }}! You can create one in your
+                    It looks like you're not part of any {{ Spark::teamString() }}!
                 </p>
                 <div class="intro-btn">
-                    <a href="/settings#/{{ str_plural(Spark::teamString()) }}" class="btn btn-outline-dark">Create Team</a>
+                    <a href="/settings#/{{ str_plural(Spark::teamString()) }}" class="btn btn-outline-dark">Create {{ Spark::teamString() }}</a>
                 </div>
             </div>
         </div>
